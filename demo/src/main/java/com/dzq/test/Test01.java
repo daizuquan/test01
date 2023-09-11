bbca1a9df08eb497fd877bf773a05a266f205f1e
daizuquan <2235795389@qq.com>
2023-09-11 10:12:27 +0800

    123

---
+++ demo/src/main/java/com/dzq/test/Test01.java
@@ -0,0 +1,12 @@
+package com.dzq.test;
+
+/**
+ * @author: daizuquan
+ * @date: 2023/9/8 17:26
+ * @description:
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
